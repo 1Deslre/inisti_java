2b50e9a272f00e90957a463f0ef9327d901485f3
1Deslre <2135232170@qq.com>
2024-03-13 22:40:06 +0800

    一些测试类

--- filedownload/src/main/java/com/deslre/filedownload/FiledownloadApplication.java
+++ filedownload/src/main/java/com/deslre/filedownload/FiledownloadApplication.java
@@ -3,10 +3,16 @@ package com.deslre.filedownload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
 public class FiledownloadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FiledownloadApplication.class, args);
+    }
+
+    static {
+        System.out.println("正在加载启动!!!@");
     }
 }
